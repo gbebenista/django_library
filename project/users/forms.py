04a06a160bc74ea012fea4b3204bda8fe2398ee6
Grzegorz Bębenista <g.bebenista@interia.pl>
2020-07-16 09:57:48 +0200

    Add registration constraint only from 'doradigital.pl' address

--- project/users/forms.py
+++ project/users/forms.py
@@ -1,9 +1,21 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+from django.core.validators import EmailValidator
+from django import forms
 
 
 class UserCreation(UserCreationForm):
+    email = forms.CharField()
+
+    def clean_email(self):
+        submitted_data = self.cleaned_data.get('email')
+        domain_part = submitted_data.rsplit('@', 1)
+        if domain_part[1] == 'doradigital.pl':
+            return submitted_data
+        raise forms.ValidationError('You must register using a @doradigital.pl address')
+
 
     class Meta:
         model = User
@@ -14,4 +26,4 @@ class UserChange(UserChangeForm):
 
     class Meta:
         model = User
-        fields = ('username',)
+        fields = ('username', 'email')
