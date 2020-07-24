ac2b14b4467af0ad8093d5beb503bf74fbd64b04
Grzegorz Bębenista <g.bebenista@interia.pl>
2020-07-24 14:23:03 +0200

    Add Book and BookcaseCart model

--- bookcase/books/models.py
+++ bookcase/books/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from users.models import CustomUser
 
-# Create your models here.
+
+class Book(models.Model):
+    author_name = models.CharField(max_length=50)
+    author_lastname = models.CharField(max_length=100)
+    title = models.CharField(max_length=200)
+    publisher = models.CharField(max_length=100)
+
+
+class BookcaseCart(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    book = models.ManyToManyField(Book)
+    borrow_date = models.DateTimeField()
+    return_date = models.DateTimeField()
