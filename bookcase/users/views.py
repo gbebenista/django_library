74833007978898524f5350550ec07f345b229930
Grzegorz Bębenista <g.bebenista@interia.pl>
2020-07-24 09:25:17 +0200

    Add redirect authenticated user

--- bookcase/users/views.py
+++ bookcase/users/views.py
@@ -14,6 +14,7 @@ class RegisterView(CreateView):
 
 
 class ViewLogin(LoginView):
+    redirect_authenticated_user = True
     form_class = UserLogin
     succes_url = reverse_lazy('home')
     template_name = 'registration/login.html'
