39e33ddc082fc4d2249632d1c90b6d8bcab588d6
Grzegorz Bębenista <g.bebenista@interia.pl>
2020-08-03 13:14:14 +0200

    Add button for adding book to basket and set book.loaner_user on current user in ajax

--- bookcase/books/urls.py
+++ bookcase/books/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+
+from books import views
 from books.views import ListBookView, CreationBookView, DeleteBookView, UpdateBookView, DetailBookView
 
 urlpatterns = [
@@ -6,5 +8,7 @@ urlpatterns = [
     path('addbook', CreationBookView.as_view(), name='addbook'),
     path('deletebook/<pk>', DeleteBookView.as_view(), name='deletebook'),
     path('updatebook/<pk>', UpdateBookView.as_view(), name='updatebook'),
-    path('detailbook/<pk>', DetailBookView.as_view(), name='detailbook')
+    path('detailbook/<pk>', DetailBookView.as_view(), name='detailbook'),
+    # path('add_to_bookcart/$', AddCartView.as_view(), name='addtocart')
+    path('add_to_bookcart', views.send_to_bucket, name='addtocart')
 ]
--- bookcase/books/views.py
+++ bookcase/books/views.py
@@ -1,10 +1,13 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db.models import Q
+from django.http import JsonResponse
 from django.urls import reverse_lazy
+from django.views import View
 from django.views.generic import ListView, CreateView, DeleteView, UpdateView, DetailView, TemplateView
 
 from books.forms import CreateBookForm, UpdateBookForm
 from books.models import Book
+from users.models import CustomUser
 
 
 class ListBookView(ListView):
@@ -15,11 +18,26 @@ class ListBookView(ListView):
     def get_queryset(self):
         query = self.request.GET.get('q')
         if not query:
-            return Book.objects.all()
+            return Book.objects.all().order_by('title')
         object_list = Book.objects.filter(
-            Q(title__icontains=query) | Q(author__icontains=query) | Q(publisher__icontains=query) | Q(tags__name__icontains=query)
+            Q(title__icontains=query) | Q(author__icontains=query) | Q(publisher__icontains=query) | Q(
+                tags__name__icontains=query)
         )
-        return object_list
+        return object_list.order_by('title')
+
+
+# class AddCartView(View):
+#     model = Book
+
+def send_to_bucket(request):
+    b = Book.objects.get(id=request.POST.get('book_id'))
+    b.loaner_user = request.user
+    b.save()
+    return JsonResponse({"success": True})
+
+
+class BasketListView(ListView):
+    pass
 
 
 class CreationBookView(CreateView):
